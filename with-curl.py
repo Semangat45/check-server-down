85d8dfe16b23a632868fc64736d8e1a5ecf66805
Adhi Setyatuhu <adhist2030@gmail.com>
2023-01-17 05:55:23 +0700

    Update function name

--- with-curl.py
+++ with-curl.py
@@ -1,10 +1,10 @@
 import os
 
 
-def print_os_command(command: str):
+def print_from_command(command: str):
     p = os.popen(command).read()
     print(p)
 
 
 if __name__ == '__main__':
-    print_os_command("curl -I https://google.com | grep HTTP")
+    print_from_command("curl -I https://google.com | grep HTTP")
