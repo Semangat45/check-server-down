5c22d905d0987e11e9cbbfc7392fb49e514bb265
Adhi Setyatuhu <adhist2030@gmail.com>
2023-01-17 06:42:56 +0700

    Schedule job

--- with-curl.py
+++ with-curl.py
@@ -1,4 +1,7 @@
 import os
+import time
+
+import schedule
 
 
 def print_from_command(command: str):
@@ -6,5 +9,16 @@ def print_from_command(command: str):
     print(p)
 
 
-if __name__ == '__main__':
+def job():
     print_from_command("curl -I https://google.com | grep HTTP")
+
+
+def schedule_job():
+    schedule.every().minute.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    schedule_job()
--- with-requests.py
+++ with-requests.py
@@ -1,4 +1,6 @@
 import requests  # pip install requests
+import schedule  # pip install schedule
+import time
 
 
 def print_status_code(url: str):
@@ -9,5 +11,16 @@ def print_status_code(url: str):
         print("RTO")
 
 
-if __name__ == "__main__":
+def job():
     print_status_code("https://google.com")
+
+
+def schedule_job():
+    schedule.every().minute.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    schedule_job()
